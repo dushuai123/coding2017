62ab1e3d5dccc7f5410ccb851b82879a08a77535
silencehe09 <silencehe09@gmail.com>
2017-02-19 12:30:00 +0800

    add consts dir
    add consts dir 、ConfigConsts

--- group19/709960951/src/main/TestUtil.java
+++ group19/709960951/src/main/TestUtil.java
@@ -4,7 +4,8 @@ public class TestUtil {
 	public static void main(String[] argv)
 	{
 		System.out.println("Hello World!");
+		System.out.println("Hello World!Github Desktop!");
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+		System.out.println("Github DeskTop!");
 	}
 }
